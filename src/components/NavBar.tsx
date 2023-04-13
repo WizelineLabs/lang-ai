89727e14c285c58e8580ee1d72649b5fa378e08c
Erick Daniel Padilla <erickdelpavo@gmail.com>
2023-04-12 18:26:32 -0600

    New NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,198 +1,123 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
+import * as React from "react";
+import { Fragment } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Menu, Transition } from "@headlessui/react";
+import {
+  ChevronDownIcon,
+  ArrowLeftOnRectangleIcon,
+} from "@heroicons/react/20/solid";
 
-const NavBar: React.FC = (props) => {
+interface NavBarLink {
+  title: string;
+  href: string;
+}
+
+const userOptions: NavBarLink[] = [
+  {
+    title: "Home",
+    href: "/dashboard",
+  },
+  {
+    title: "Learn",
+    href: "/learn",
+  },
+  {
+    title: "Evaluations",
+    href: "/evaluations",
+  },
+  {
+    title: "Grades",
+    href: "/grades",
+  },
+];
+
+function NavBar() {
+  function didTapLogOut() {
+    console.log("Logged Out");
+  }
+
   return (
-    <nav
-      className="flex-no-wrap relative flex w-full items-center justify-between bg-neutral-100 py-4 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start"
-      data-te-navbar-ref>
-      <div className="flex w-full flex-wrap items-center justify-between px-6">
-        <button
-          className="block border-0 bg-transparent py-2 px-2.5 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-          type="button"
-          data-te-collapse-init
-          data-te-target="#navbarSupportedContent1"
-          aria-controls="navbarSupportedContent1"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="[&>svg]:w-7">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-7 w-7">
-              <path
-                fill-rule="evenodd"
-                d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                clip-rule="evenodd" />
-            </svg>
-          </span>
-        </button>
-        <div
-          className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
-          id="navbarSupportedContent1"
-          data-te-collapse-item>
-          <a
-            className="mt-2 mr-2 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mt-0"
-            href="#">
-            <img
-              src="/logomark_red_X2.png"
-              style={{ height: 15 }}
-              alt=""
-              loading="lazy" />
-          </a>
-          <ul
-            className="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
-            data-te-navbar-nav-ref>
-            <li className="lg:pr-2" data-te-nav-item-ref>
-              <a
-                className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
-                href="/dashboard"
-                data-te-nav-link-ref
-                >Home
-              </a>
-            </li>
-            <li className="lg:pr-2" data-te-nav-item-ref>
-              <a
-                className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                href="/learn"
-                data-te-nav-link-ref
-                >Learn
-              </a>
-            </li>
-            <li className="lg:pr-2" data-te-nav-item-ref>
-              <a
-                className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                href="/evaluations"
-                data-te-nav-link-ref
-                >Evaluations
-              </a>
-            </li>
-            <li className="lg:pr-2" data-te-nav-item-ref>
-              <a
-                className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                href="/grades"
-                data-te-nav-link-ref
-                >Grades
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="relative flex items-center">
-          <a
-            className="mr-4 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-            href="#">
-            <span className="[&>svg]:w-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-5 w-5">
-                <path
-                  d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-              </svg>
+    <nav className="flex flex-row place-content-between border-b bg-white">
+      <div className="mx-4 flex flex-row space-x-3">
+        <Image
+          className="my-2"
+          src="/wizeline-light.svg"
+          alt="Wizeline"
+          width={207.5}
+          height={36}
+        />
+        <span className="self-center text-3xl font-extrabold">LangAI</span>
+      </div>
+      <div className="mx-4 flex flex-row space-x-5">
+        {userOptions.map((option) => (
+          <Link
+            key={option.href}
+            href={option.href}
+            className="self-center text-base font-medium text-slate-500 hover:opacity-50"
+          >
+            {option.title}
+          </Link>
+        ))}
+        <Menu as="div" className="relative self-center">
+          <Menu.Button
+            type="button"
+            className="flex flex-row justify-center text-base font-medium text-slate-500 hover:opacity-50"
+            id="dropdownDefaultButton"
+            data-dropdown-toggle="dropdown"
+          >
+            <Image
+              className="mr-2 rounded-full"
+              src="/bismarck.jpg"
+              alt="Profile Picture"
+              width={32}
+              height={32}
+            />
+            <span className="inline-flex flex-row self-center">
+              Bismarck
+              <ChevronDownIcon
+                className="-mr-1 h-6 w-6 text-slate-500"
+                aria-hidden="true"
+              />
             </span>
-          </a>
-          <div className="relative" data-te-dropdown-ref>
-            <a
-              className="hidden-arrow mr-4 flex items-center text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-              href="#"
-              id="dropdownMenuButton1"
-              role="button"
-              data-te-dropdown-toggle-ref
-              aria-expanded="false">
-              <span className="[&>svg]:w-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5">
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
-                    clip-rule="evenodd" />
-                </svg>
-              </span>
-              <span
-                className="absolute -mt-2.5 ml-2 rounded-full bg-red-700 py-0 px-1.5 text-xs text-white"
-                >1</span
-              >
-            </a>
-            <ul
-              className="absolute left-auto right-0 z-[1000] float-left m-0 mt-1 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-              aria-labelledby="dropdownMenuButton1"
-              data-te-dropdown-menu-ref>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                  >Action</a
-                >
-              </li>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                  >Another action</a
-                >
-              </li>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                  >Something else here</a
-                >
-              </li>
-            </ul>
-          </div>
-          <div className="relative" data-te-dropdown-ref>
-            <a
-              className="hidden-arrow flex items-center whitespace-nowrap transition duration-150 ease-in-out motion-reduce:transition-none"
-              href="#"
-              id="dropdownMenuButton2"
-              role="button"
-              data-te-dropdown-toggle-ref
-              aria-expanded="false">
-              <img
-                src="https://tecdn.b-cdn.net/img/new/avatars/2.jpg"
-                className="rounded-full"
-                style={{ width: 25, height: 25 }}
-                alt=""
-                loading="lazy" />
-            </a>
-            <ul
-              className="absolute left-auto right-0 z-[1000] float-left m-0 mt-1 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-              aria-labelledby="dropdownMenuButton2"
-              data-te-dropdown-menu-ref>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                  >Action
-                </a>
-              </li>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                  >Another action</a>
-              </li>
-              <li>
-                <a
-                  className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                  href="#"
-                  data-te-dropdown-item-ref
-                  >Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-4 py-2">
+                <div className="text-sm text-slate-900">Bismarck Lepe</div>
+                <div className="truncate text-sm font-medium text-slate-600">
+                  bismarck@wizeline.com
+                </div>
+              </div>
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active: hovered }) => (
+                    <button
+                      onClick={() => didTapLogOut()}
+                      className={`${
+                        hovered ? "bg-slate-100" : "bg-white text-gray-900"
+                      } group flex w-full items-center px-3 py-2 text-sm text-slate-900`}
+                    >
+                      <ArrowLeftOnRectangleIcon
+                        className="mr-2 h-5 w-5 text-red-500"
+                        aria-hidden="true"
+                      />
+                      Sign out
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
     </nav>
   );
