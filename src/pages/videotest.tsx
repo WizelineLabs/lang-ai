72b2880ec4a233e178ad77b3d00ba819bf36f68e
JoseAKaun <joseakaun@me.com>
2023-04-13 19:17:22 -0600

    pagina para probar webcam recording, solo el file

---
+++ src/pages/videotest.tsx
@@ -0,0 +1,9 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import NavBar from "~/components/NavBar";
+import PageTitle from "~/components/PageTitle";
+import PageWrapper from "~/components/PageWrapper";
+import Button from "~/components/Button";
+import Section from "~/components/Section";
+
+import { api } from "~/utils/api";
