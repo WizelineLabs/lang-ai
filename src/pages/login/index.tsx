56adab1dca44a9699eb334c91789d54823815ba1
HumbertoRdz <a01197898@itesm.mx>
2023-04-19 17:08:30 -0600

    Login front

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -14,12 +14,13 @@ const Home: NextPage = () => {
       
       
        
-          <div className="g-0 lg:flex lg:flex-wrap max-h-fullscreen " >
+          <div className="g-0 lg:flex lg:flex-wrap h-screen " >
             
             <div className="lg:w-6/12">
               <div className="mx-6 p-12">
                 
                 <div className="text-center">
+                  <br /><br /><br /><br /><br /><br /><br />
                   <img
                     className="mx-auto w-80"
                     src="/wizeline-light.svg"
@@ -43,7 +44,7 @@ const Home: NextPage = () => {
                       placeholder="Username" />
                     <label
                       htmlFor="exampleFormControlInput1"
-                      className="text-xl pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                      className="text-xl pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-500 dark:peer-focus:text-primary"
                       >Username
                     </label>
                   </div>
@@ -57,7 +58,7 @@ const Home: NextPage = () => {
                       placeholder="Password" />
                     <label
                       htmlFor="exampleFormControlInput11"
-                      className="text-xl pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                      className="text-xl pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-500 dark:peer-focus:text-primary"
                       >Password
                     </label>
                   </div>
@@ -95,7 +96,7 @@ const Home: NextPage = () => {
 
             
             <div
-              className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+              className="flex items-center  lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
               style={{background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)"}}>
               <div className="px-4 py-6 text-white mx-6 p-12">
                 <h4 className="mb-6 text-4xl font-semibold">
