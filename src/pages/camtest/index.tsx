e5f8993919779d7473f1b94ac40d0e79ed6aa835
JoseAKaun <joseakaun@me.com>
2023-04-24 15:12:21 -0600

    Camera Test Commit
    Still no audio, and needs cleaning up.

--- src/pages/camtest/index.tsx
+++ src/pages/camtest/index.tsx
@@ -1,12 +1,82 @@
 import { type NextPage } from "next";
+import React from "react";
 import PageTitle from "~/components/PageTitle";
 import PageWrapper from "~/components/PageWrapper";
 import Button from "~/components/Button";
 import Section from "~/components/Section";
+import Webcam from "react-webcam"
+import { useRef,useState, useCallback } from "react";
+//import { MediaRecorderErrorEvent, MediaRecorderDataAvailableEvent } from 'dom-mediacapture-record';
 
-import { api } from "~/utils/api";
+
+const videoConstraints = {
+  audio: true,
+  video: true,
+  width: 720,
+  height: 360,
+  facingmode: "user"
+}
+
 
 const Camtest: NextPage = () => {
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+
+  const webcamRef = useRef<Webcam>(null);
+  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+  const [recordedVideoUrl, setRecordedVideoUrl] = useState<string | null>(
+    null
+  );
+
+  const handleRecord = () => {
+    if (!mediaRecorder) {
+      // Create a new MediaRecorder object with the desired settings
+      const stream = webcamRef.current?.stream;
+      if (!stream) {
+        return;
+      }
+      const mediaRecorder = new MediaRecorder(stream, {
+        mimeType: "video/webm;codecs=vp9,opus",
+        audioBitsPerSecond: 128000
+      });
+      setMediaRecorder(mediaRecorder);
+
+      // Register event listener for the MediaRecorder object
+      mediaRecorder.addEventListener("dataavailable", (event: any) => {
+        if (event.data.size > 0) {
+          // Create a Uint8Array buffer from the Blob data
+          const reader = new FileReader();
+          reader.onload = () => {
+            const buffer = new Uint8Array(reader.result as ArrayBuffer);
+            const blob = new Blob([buffer], { type: "video/webm;codecs=vp9,opus" });
+
+            // Add the recorded data to the recordedChunks state variable
+            setRecordedChunks((prevChunks) => [...prevChunks, blob]);
+          };
+          reader.readAsArrayBuffer(event.data);
+        }
+      });
+
+      // Start recording
+      mediaRecorder.start();
+    } else {
+      // Stop recording
+      mediaRecorder.stop();
+      setMediaRecorder(null);
+      concatenateRecordedChunks();
+    }
+  };
+
+  const concatenateRecordedChunks = () => {
+    const recordedBlob = new Blob(recordedChunks, { type: "video/webm;codecs=vp9,opus" });
+    setRecordedVideoUrl(URL.createObjectURL(recordedBlob));
+    setRecordedChunks([]);
+  };
+
+  
+  
+  
   return (
     <>
       <PageWrapper>
@@ -27,20 +97,16 @@ const Camtest: NextPage = () => {
           </div>
 
           <br />
-          <div
-            className="flex flex-col p-5"
-            style={{
-              margin: "auto",
-              width: 400,
-              height: 300,
-              background: "black",
-            }}
-          ></div>
-          {/* Remplazar este div con la webcam */}
+          <div>
+          <Webcam videoConstraints={videoConstraints} ref={webcamRef}/>
+          </div>
+         
           <Section>
             <div style={{ width: "75%", float: "left" }}>
               <div style={{ width: "50%", float: "left", padding: 20 }}>
-                <Button>Record</Button> {/* Remplazar boton por icono record*/}
+                <button onClick={handleRecord}>
+                {mediaRecorder ? 'Stop Recording' : 'Start Recording'}
+                </button>                
               </div>
               <div style={{ width: "45%", float: "right", padding: 20 }}>
                 <Button>Volumen</Button>
@@ -52,9 +118,22 @@ const Camtest: NextPage = () => {
             </div>
           </Section>
         </div>
+        <br>
+        </br>
+        <div style={{float: "left", padding: 100}}>
+          {recordedChunks.length > 0 && (
+            <button onClick={concatenateRecordedChunks} style={{float: "left", paddingLeft: 250}}>See results</button>
+          )}
+          {recordedVideoUrl && (
+            <video controls src={recordedVideoUrl} />
+          )}
+        </div>
       </PageWrapper>
     </>
   );
 };
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 export default Camtest;
