75025c017864c20a351f7b0868a1c421f47c88a2
Erick Daniel Padilla <erickdelpavo@gmail.com>
2023-04-25 15:44:50 -0600

    Audio recording fix

--- src/pages/camtest/index.tsx
+++ src/pages/camtest/index.tsx
@@ -4,83 +4,125 @@ import PageTitle from "~/components/PageTitle";
 import PageWrapper from "~/components/PageWrapper";
 import Button from "~/components/Button";
 import Section from "~/components/Section";
-import Webcam from "react-webcam"
-import { useRef,useState, useCallback } from "react";
+import Webcam from "react-webcam";
+import { useRef, useState, useCallback } from "react";
+import Link from "next/link";
 //import { MediaRecorderErrorEvent, MediaRecorderDataAvailableEvent } from 'dom-mediacapture-record';
-
 
 const videoConstraints = {
   audio: true,
   video: true,
-  width: 720,
-  height: 360,
-  facingmode: "user"
-}
+  width: 1280,
+  height: 720,
+  facingmode: "user",
+};
 
+type VideoFormat = {
+  ext: string;
+  mimeType: string;
+  name: string;
+};
+
+const formats: VideoFormat[] = [
+  { ext: "webm", mimeType: 'video/webm;codecs="vp9"', name: "VP9 (webm)" },
+  { ext: "webm", mimeType: 'video/webm;codecs="vp8"', name: "VP8 (webm)" },
+  { ext: "webm", mimeType: "video/webm;codecs=h264", name: "H.264 (webm)" },
+  {
+    ext: "mp4",
+    mimeType: 'video/mp4;codecs="avc1.42E01E"',
+    name: "H.264 (mp4)",
+  },
+];
+
+const getVideoFormat = () => {
+  for (const format of formats) {
+    if (MediaRecorder.isTypeSupported(format.mimeType)) {
+      console.log(`Codec ${format.name} will be used`);
+      return format;
+    }
+  }
+  console.error("No codecs supported by browser");
+  return undefined;
+};
 
 const Camtest: NextPage = () => {
+  const [videoFormat, setVideoFormat] = useState<VideoFormat | undefined>(
+    undefined
+  );
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
     null
   );
 
   const webcamRef = useRef<Webcam>(null);
   const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
-  const [recordedVideoUrl, setRecordedVideoUrl] = useState<string | null>(
-    null
-  );
+  const [recordedVideoUrl, setRecordedVideoUrl] = useState<string | null>(null);
 
   const handleRecord = () => {
     if (!mediaRecorder) {
-      // Create a new MediaRecorder object with the desired settings
+      console.log("Will start recording");
+
+      // Only continue if the webcam is being shown on the screen
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       const stream = webcamRef.current?.stream;
-      if (!stream) {
-        return;
-      }
+      if (!stream) return;
+
+      // Get video format for video file
+      const videoFormat = getVideoFormat();
+      const videoCodec = videoFormat?.mimeType;
+      setVideoFormat(videoFormat);
+
+      // Create a new MediaRecorder object with the desired settings
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       const mediaRecorder = new MediaRecorder(stream, {
-        mimeType: "video/webm;codecs=vp9,opus",
-        audioBitsPerSecond: 128000
+        mimeType: videoCodec,
+        videoBitsPerSecond: 1500000, // 1500 kbps
+        audioBitsPerSecond: 320000, // 320 kbps
       });
       setMediaRecorder(mediaRecorder);
 
       // Register event listener for the MediaRecorder object
-      mediaRecorder.addEventListener("dataavailable", (event: any) => {
+      mediaRecorder.ondataavailable = (event) => {
+        console.log("MediaRecorder has new data available");
         if (event.data.size > 0) {
           // Create a Uint8Array buffer from the Blob data
           const reader = new FileReader();
           reader.onload = () => {
             const buffer = new Uint8Array(reader.result as ArrayBuffer);
-            const blob = new Blob([buffer], { type: "video/webm;codecs=vp9,opus" });
+            const blob = new Blob([buffer], {
+              type: videoCodec,
+            });
 
             // Add the recorded data to the recordedChunks state variable
             setRecordedChunks((prevChunks) => [...prevChunks, blob]);
+            console.log("Added chunk to video");
           };
           reader.readAsArrayBuffer(event.data);
         }
-      });
+      };
 
       // Start recording
       mediaRecorder.start();
     } else {
+      console.log("Will stop recording");
       // Stop recording
       mediaRecorder.stop();
       setMediaRecorder(null);
-      concatenateRecordedChunks();
     }
   };
 
   const concatenateRecordedChunks = () => {
-    const recordedBlob = new Blob(recordedChunks, { type: "video/webm;codecs=vp9,opus" });
+    const recordedBlob = new Blob(recordedChunks, {
+      type: videoFormat?.mimeType,
+    });
+    console.log("Created file");
     setRecordedVideoUrl(URL.createObjectURL(recordedBlob));
     setRecordedChunks([]);
   };
 
-  
-  
-  
   return (
     <>
       <PageWrapper>
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-6">
           <div className="flex flex-row justify-between">
             <div className="flex flex-col">
               <PageTitle editsTitle>Webcam Test</PageTitle>
@@ -90,50 +132,50 @@ const Camtest: NextPage = () => {
             </div>
 
             <div className="my-auto flex flex-row space-x-3">
-              <a href="/exercise">
+              <Link href="/exercise">
                 <Button>Next</Button>
-              </a>
+              </Link>
             </div>
           </div>
 
-          <br />
-          <div>
-          <Webcam videoConstraints={videoConstraints} ref={webcamRef}/>
-          </div>
-         
+          <Webcam
+            className="aspect-video w-full rounded-lg border bg-black"
+            videoConstraints={videoConstraints}
+            muted={true}
+            audio={true}
+            mirrored={true}
+            ref={webcamRef}
+          />
+
           <Section>
-            <div style={{ width: "75%", float: "left" }}>
-              <div style={{ width: "50%", float: "left", padding: 20 }}>
-                <button onClick={handleRecord}>
-                {mediaRecorder ? 'Stop Recording' : 'Start Recording'}
-                </button>                
-              </div>
-              <div style={{ width: "45%", float: "right", padding: 20 }}>
-                <Button>Volumen</Button>
-              </div>
-            </div>
-            <div style={{ width: "15%", float: "right", padding: 20 }}>
-              <Button>Redo</Button>
-              {/* Remplazar boton por icono redo*/}
+            <div className="flex flex-row justify-between p-5">
+              <Button onClick={() => handleRecord()}>
+                {mediaRecorder ? "Stop Recording" : "Start Recording"}
+              </Button>
+              <Button
+                onClick={() => concatenateRecordedChunks()}
+                hidden={
+                  !!mediaRecorder ||
+                  !!recordedVideoUrl ||
+                  recordedChunks.length === 0
+                }
+              >
+                Create file for download
+              </Button>
+              <a
+                className="mx-3 my-auto"
+                href={recordedVideoUrl ?? undefined}
+                download={"WebcamTest." + (videoFormat?.ext ?? "mp4")}
+                hidden={!recordedVideoUrl}
+              >
+                Download
+              </a>
             </div>
           </Section>
-        </div>
-        <br>
-        </br>
-        <div style={{float: "left", padding: 100}}>
-          {recordedChunks.length > 0 && (
-            <button onClick={concatenateRecordedChunks} style={{float: "left", paddingLeft: 250}}>See results</button>
-          )}
-          {recordedVideoUrl && (
-            <video controls src={recordedVideoUrl} />
-          )}
         </div>
       </PageWrapper>
     </>
   );
 };
-function delay(ms: number) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
-}
 
 export default Camtest;
