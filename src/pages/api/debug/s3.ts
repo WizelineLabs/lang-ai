f43b6fd14b11ba3f4221b98d851593f129bfaf5c
Erick Daniel Padilla <erickdelpavo@gmail.com>
2023-05-02 22:42:55 -0600

    Added S3 file upload

---
+++ src/pages/api/debug/s3.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { readFile } from "fs/promises";
+import { uploadFile } from "~/services/aws/s3";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const path = req.query.path as string;
+    const mimeType = req.query.mimeType as string | undefined;
+
+    const buffer = await readFile(path);
+    await uploadFile(buffer, "test/hello.m4a");
+    res.status(200).json({ done: true });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
---
+++ src/server/api/routers/files.ts
@@ -0,0 +1,33 @@
+import { randomUUID } from "crypto";
+import path from "path";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+import { uploadFile } from "~/services/aws/s3";
+
+export const filesRouter = createTRPCRouter({
+  saveFile: publicProcedure
+    .input(
+      z.object({
+        base64String: z.string(),
+        mimeType: z.string(),
+        extension: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const fileName = randomUUID() + "." + input.extension;
+      const filePath = path.join("test", fileName);
+
+      const buffer = Buffer.from(input.base64String);
+      await uploadFile(buffer, filePath);
+
+      return {
+        message: "File uploaded",
+      };
+    }),
+});
