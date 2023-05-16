c7297009da8c89e10b8351c0d01168d47a463107
Erick Daniel Padilla <erickdelpavo@gmail.com>
2023-05-16 10:47:49 -0600

    Fix env + prisma schema (auth fixed)

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,16 +4,13 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import DiscordProvider from "next-auth/providers/discord";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import Auth0Provider from "next-auth/providers/auth0";
-
-
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -55,39 +52,44 @@ export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      id: 'fake-user',
+      id: "fake-user",
       name: "Fake User",
       // `credentials` is used to generate a form on the sign in page.
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "john@example.com" },
-        password: { label: "Password", type: "password" }
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "john@example.com",
+        },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        await delay(1000)
+        await delay(1000);
 
         // Add logic here to look up the user from the credentials supplied
-        if (credentials
-          && credentials.username === 'erick@example.com'
-          && credentials.password === '12345678'
+        if (
+          credentials &&
+          credentials.username === "erick@example.com" &&
+          credentials.password === "12345678"
         ) {
-          const user = { id: "1", name: "Erick Padilla", email: "erick@example.com" }
+          const user = {
+            id: "1",
+            name: "Erick Padilla",
+            email: "erick@example.com",
+          };
           // Any object returned will be saved in `user` property of the JWT
-          return user
+          return user;
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
-          return null
-  
+          return null;
+
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
-      }
+      },
     }),
-    // DiscordProvider({
-    //   clientId: env.DISCORD_CLIENT_ID,
-    //   clientSecret: env.DISCORD_CLIENT_SECRET,
-    // }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
@@ -97,7 +99,7 @@ export const authOptions: NextAuthOptions = {
     Auth0Provider({
       clientId: env.AUTH0_CLIENT_ID,
       clientSecret: env.AUTH0_CLIENT_SECRET,
-      issuer: env.AUTH0_ISSUER
+      issuer: env.AUTH0_ISSUER,
     }),
 
     /**
@@ -113,7 +115,7 @@ export const authOptions: NextAuthOptions = {
 };
 
 async function delay(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /**
