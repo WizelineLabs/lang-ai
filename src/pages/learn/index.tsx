a747c621806db7b4dafca7828e3d6b36b569880a
Erick Daniel Padilla <erickdelpavo@gmail.com>
2023-05-16 21:08:33 -0600

    LessonRow + Example

--- src/pages/learn/index.tsx
+++ src/pages/learn/index.tsx
@@ -1,12 +1,14 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import { useState } from "react";
-import NavBar from "~/components/NavBar";
-import PageTitle from "~/components/PageTitle";
-import PageWrapper from "~/components/PageWrapper";
-import Section from "~/components/Section";
-import SegmentedPicker from "~/components/SegmentedPicker";
-import { Dropdown, DropdownButton } from "~/components/Dropdown";
+import {
+  PageTitle,
+  PageWrapper,
+  Section,
+  SegmentedPicker,
+  Dropdown,
+  DropdownButton,
+} from "~/components";
+import { LessonRow } from "~/components/tables";
 
 import { api } from "~/utils/api";
 
@@ -46,87 +48,25 @@ const Learn: NextPage = () => {
             </Dropdown>
           </div>
           <Section title="Part 1. Part title...">
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-              <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-                <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Title
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Description
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Difficulty
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      <span className="sr-only">Start</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-                    >
-                      Lesson Title
-                    </th>
-                    <td className="px-6 py-4">Lesson Description</td>
-                    <td className="px-6 py-4" style={{ color: "green" }}>
-                      Easy
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                      >
-                        Start
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-                    >
-                      Lesson Title
-                    </th>
-                    <td className="px-6 py-4">Lesson Description</td>
-                    <td className="px-6 py-4" style={{ color: "green" }}>
-                      Easy
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                      >
-                        Start
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-                    >
-                      Lesson Title
-                    </th>
-                    <td className="px-6 py-4">Lesson Description</td>
-                    <td className="px-6 py-4" style={{ color: "green" }}>
-                      Easy
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                      >
-                        Start
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className="space-0 flex flex-col divide-y">
+              <LessonRow
+                title="Test"
+                description="Hola"
+                difficulty={0}
+                state="done"
+              />
+              <LessonRow
+                title="Test"
+                description="Hola"
+                difficulty={1}
+                state="inProgress"
+              />
+              <LessonRow
+                title="Test"
+                description="Hola"
+                difficulty={2}
+                state="pending"
+              />
             </div>
           </Section>
           <Section title="Part 2. Part title...">
