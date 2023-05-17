8004397ef14a7e88c0c7baa9a7974c441d369cbe
Erick Daniel Padilla <erickdelpavo@gmail.com>
2023-05-16 21:06:46 -0600

    Button themes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,48 +1,90 @@
 import * as React from "react";
-import Link, { LinkProps } from "next/link";
+import Link, { type LinkProps } from "next/link";
 
 const buttonClassName =
-  "rounded-md bg-red-600 px-3 py-2 text-base font-medium tracking-wide text-white hover:bg-red-700 hover:text-gray-100 focus:outline-none focus:ring focus:ring-red-600/50 focus:ring-offset-0";
+  "rounded-md px-3 py-2 text-base tracking-wide focus:outline-none focus:ring focus:ring-offset-0 shadow-sm inline-flex items-center align-middle gap-1.5";
 
-// interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+type ButtonTheme = "primary" | "primary-inverted" | "secondary";
 
-export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+function getClassNameForTheme(theme: ButtonTheme) {
+  switch (theme) {
+    case "primary":
+      return `${buttonClassName} bg-red-600 hover:bg-red-700 text-white hover:text-gray-100 font-medium focus:ring-red-600/50 border border-red-600`;
+    case "primary-inverted":
+      return `${buttonClassName} bg-white hover:bg-slate-50 text-red-600 hover:text-red-700 font-regular focus:ring-red-600/50 border border-slate-200`;
+    case "secondary":
+      return `${buttonClassName} bg-white hover:bg-slate-50 text-slate-700 hover:text-slate-900 font-regular focus:ring-slate-700/50 border border-slate-200`;
+  }
+}
+
+interface MainButtonProps {
+  theme?: ButtonTheme;
+  icon?: React.ReactNode;
+  iconInRight?: boolean;
+}
+
+interface ButtonProps
+  extends MainButtonProps,
+    React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button(props: ButtonProps) {
+  const theme = props.theme ?? "primary";
   return (
     <button
       {...props}
-      className={`${buttonClassName} ${props.className ?? ""}`}
+      className={`${buttonClassName} ${getClassNameForTheme(theme)} ${
+        props.className ?? ""
+      }`}
     >
+      {!props.iconInRight && props.icon}
       <span>{props.children}</span>
+      {props.iconInRight && props.icon}
     </button>
   );
 }
 
+export default Button;
+
 export interface LinkButtonProps
-  extends LinkProps,
+  extends MainButtonProps,
+    LinkProps,
     React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
   asAnchor?: boolean;
 }
 
 export function LinkButton(props: LinkButtonProps) {
+  const theme = props.theme ?? "primary";
   if (props.asAnchor) {
     return (
       <a
         {...props}
-        className={`${buttonClassName} py-2.5 ${props.className ?? ""}`}
+        className={`${buttonClassName} ${getClassNameForTheme(theme)} ${
+          props.className ?? ""
+        }`}
       >
+        {!props.iconInRight && props.icon}
         <span>{props.children}</span>
+        {props.iconInRight && props.icon}
       </a>
     );
   }
   return (
     <Link
       {...props}
-      className={`${buttonClassName} py-2.5 ${props.className ?? ""}`}
+      className={`${buttonClassName} ${getClassNameForTheme(theme)} ${
+        props.className ?? ""
+      }`}
     >
+      {!props.iconInRight && props.icon}
       <span>{props.children}</span>
+      {props.iconInRight && props.icon}
     </Link>
   );
 }
 
-export default Button;
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+export function ChevronIcon() {
+  return <ChevronRightIcon className="-mx-1 h-5" strokeWidth={2} aria-hidden />;
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: ["15px", "20px"],
+        base: ["17px", "24px"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: colors.slate[950],
+          dark: colors.white,
+        },
+        secondary: {
+          DEFAULT: colors.slate[600],
+          dark: colors.slate[400],
+        },
+      },
+    },
   },
   plugins: [],
 };
