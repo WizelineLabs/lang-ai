cdae401aba6555594dc2b18dbcc361ea04e96932
JoseAKaun <joseakaun@me.com>
2023-05-17 22:47:48 -0600

    Audio Exercise
    Progress bar, with longer exercise audio.

--- src/pages/exercise-listen/index.tsx
+++ src/pages/exercise-listen/index.tsx
@@ -8,6 +8,7 @@ import InstructionAudio from "~/components/test/InstructionAudio";
 import ResponseText from "~/components/test/ResponseText";
 
 const ExerciseListen: NextPage = () => {
+  const audioUrl = "sounds/bandaudition.mp3";
   return (
     <>
       <PageWrapper>
@@ -20,13 +21,12 @@ const ExerciseListen: NextPage = () => {
             <div className="my-auto flex flex-row space-x-5">
               <h2 className="py-3 font-sans">1 of 10</h2>
               <Button>Previous</Button>
-
               <Button>Next</Button>
             </div>
           </div>
           <Section>
             <div className="flex flex-col gap-4 px-4 py-3">
-              <InstructionAudio></InstructionAudio>
+              <InstructionAudio audioUrl={audioUrl} />
               <ResponseText></ResponseText>
             </div>
           </Section>
