6e3d9d7d480f8805231403f8def8c3f751be99ea
rgg2001 <78384733+rgg2001@users.noreply.github.com>
2023-05-17 23:27:04 -0600

    Detalles exercises

--- src/pages/exercise-listen/index.tsx
+++ src/pages/exercise-listen/index.tsx
@@ -15,12 +15,11 @@ const ExerciseListen: NextPage = () => {
         <div className="flex flex-col">
           <div className="flex flex-row justify-between">
             <div className="flex flex-col">
-              <PageTitle editsTitle>Lesson Title</PageTitle>
+              <PageTitle editsTitle>Listening Exercise</PageTitle>
             </div>
 
             <div className="my-auto flex flex-row space-x-5">
               <h2 className="py-3 font-sans">1 of 10</h2>
-              <Button>Previous</Button>
               <Button>Next</Button>
             </div>
           </div>
