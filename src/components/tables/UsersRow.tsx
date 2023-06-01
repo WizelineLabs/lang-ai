5afc51dba5057872ee077ccbe35c9a31c877b357
rgg2001 <78384733+rgg2001@users.noreply.github.com>
2023-05-31 20:10:46 -0600

    UsersRow Acomodado Izq

--- src/components/tables/UsersRow.tsx
+++ src/components/tables/UsersRow.tsx
@@ -22,26 +22,29 @@ export function UsersRow(props: UsersRowProps) {
       <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
         <tbody>
           <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-            <th scope="row" className="px-6 py-4 dark:text-white ">
-              {image ? (
-                <img
-                  className="ml-3 rounded-full"
-                  src={image}
-                  alt="Profile Picture"
-                  width={40}
-                  height={40}
-                />
-              ) : (
-                <Image
-                  className="ml-3 rounded-full"
-                  src="/defaultuser.png"
-                  alt="Profile Picture"
-                  width={40}
-                  height={40}
-                />
-              )}
-            </th>
-            <td className="px-6 py-6">{name}</td>
+            <td className="flex items-start items-center px-6 py-4">
+              <span className="flex-shrink-0">
+                {image ? (
+                  <img
+                    className="ml-3 rounded-full"
+                    src={image}
+                    alt="Profile Picture"
+                    width={40}
+                    height={40}
+                  />
+                ) : (
+                  <Image
+                    className="ml-3 rounded-full"
+                    src="/defaultuser.png"
+                    alt="Profile Picture"
+                    width={40}
+                    height={40}
+                  />
+                )}
+              </span>
+              <span className="ml-4">{name}</span>
+            </td>
+
             <td className="px-6 py-4 text-right">
               <LinkButton
                 className="font-medium text-blue-600 hover:underline dark:text-blue-500"
